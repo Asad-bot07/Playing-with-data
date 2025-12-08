6c4a9cb2ac932ba554243dab3a8b3df1fd6f9ebf
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-08 23:58:07 +0530

    Prac 10 added with random seeding

--- Numpy/10Prac.py
+++ Numpy/10Prac.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+# random seeding
+np.random.seed(24)
+
 # from 0 to 1
 random_array = np.random.rand(3, 4)
 print(random_array)
@@ -7,4 +10,3 @@ print(random_array)
 # real numbers
 random_integers = np.random.randint(0, 10, size=[2, 3])
 print(random_integers)
-
