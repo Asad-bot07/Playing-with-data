3ebb56e39372b91b9f3811620cfa42932a5186c9
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-09 00:35:34 +0530

    Practice 11 added, broadcasting operations

---
+++ Numpy/11Prac.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# broadcasting operations
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+vector = np.array([1,4,8])
+
+# addition
+print(matrix + vector)
+
+# multiplication
+print(matrix * vector)
