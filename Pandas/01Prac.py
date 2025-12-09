5ced26db27a4cf2258451924cfc9681a06405d81
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-09 17:14:58 +0530

    Started with pandas

---
+++ Pandas/01Prac.py
@@ -0,0 +1,5 @@
+# Pandas is a powerful python libraries for data manipulation and analysis which provides data structure as Series and Data frames
+
+# Series is an one dimensional data structure with labels which can store any data set
+
+# data frames is a 2 dimensional data structrue
