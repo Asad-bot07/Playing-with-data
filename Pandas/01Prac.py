f19d9e1f81cd44ee91dde246057d0c06d640f510
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-09 17:28:02 +0530

    introduction to pandas, series and dataframe eg

--- Pandas/01Prac.py
+++ Pandas/01Prac.py
@@ -1,5 +1,17 @@
+import pandas as pd
+
 # Pandas is a powerful python libraries for data manipulation and analysis which provides data structure as Series and Data frames
 
 # Series is an one dimensional data structure with labels which can store any data set
 
-# data frames is a 2 dimensional data structrue
+# data frames is a 2 dimensional labeled data structrue
+
+# series
+series = pd.Series([10, 20, 30], index=["a", "b", "c"])
+print(series)
+
+# dataframe
+data = {"name" : ["Asad", "Ayushi"], "Age": [20, 21]}
+# print(data)
+df = pd.DataFrame(data)
+print(df)
