a1bd2418f8172b5d50227aed0335e6641f935d3c
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-12 00:04:03 +0530

    more func added in 04Prac regarding handlind missing data

--- Pandas/04Prac.py
+++ Pandas/04Prac.py
@@ -8,4 +8,13 @@ df = df.dropna() #it will drop rows with missing values
 
 df = df.dropna(axis=1) # it will drop column with missign values
 
-df["column_name"] = df["column_name"].fillna(0) # this will fill the columns which have missing values with 0 as we need the column and cannot just drop it
+df["column_name"] = df["column_name"].fillna(0) # this will fill the columns which have missing values with 0 or any numbers as we need the column and cannot just drop it
+
+# forward fill
+df = df.fillna(method="ffill")
+
+# backward fill
+df = df.fillna(method="bfill")
+
+# interpolation way
+df["column_name"] = df["column_name"].interpolate()
