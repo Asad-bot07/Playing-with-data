996108ee4889d3b218c46c4af069c5a5778fe645
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-11 23:55:28 +0530

    04Pract with data cleaning, dropna() func and fillna()

---
+++ Pandas/04Prac.py
@@ -0,0 +1,11 @@
+# handling missing values
+
+import pandas as pd
+
+df = pd.read_csv('Pandas/Data_set/MOCK_DATA.csv')
+
+df = df.dropna() #it will drop rows with missing values
+
+df = df.dropna(axis=1) # it will drop column with missign values
+
+df["column_name"] = df["column_name"].fillna(0) # this will fill the columns which have missing values with 0 as we need the column and cannot just drop it
