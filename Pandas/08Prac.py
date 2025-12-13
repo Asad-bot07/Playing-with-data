b727abd02511329ea54476f3132286d8eeb8e60a
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-13 23:22:22 +0530

    Started with Data aggregation and grouping in pandas

---
+++ Pandas/08Prac.py
@@ -0,0 +1,13 @@
+# Data aggregation and grouping
+
+import numpy as np
+import pandas as pd
+
+data1 = {'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]}
+
+data2 = {'City': ['New York', 'London', 'Paris'], 'Salary': [50000, 60000, 70000]}
+
+df = pd.DataFrame(data1)
+df1 = pd.DataFrame(data2)
+
+# print(df,"\n", df1)
