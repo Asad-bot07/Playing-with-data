f95ffcf78ed1ab496c42ed624530525385b1a10d
Asad-bot07 <asadhussain2408@gmail.com>
2025-12-18 01:15:23 +0530

    Start of matplotlib

---
+++ Matplotlib/01Prac.py
@@ -0,0 +1,11 @@
+# Matplotlib is a python library for creating static and interactive, animated graph to plot our data for data visualization
+
+# importing matplotlib
+import matplotlib.pyplot as plt
+
+# Basic coords
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [90, 70, 75, 43, 39, 24, 16, 3, 0]
+
+plt.plot(x, y) # to plot data in the graph
+plt.show() # to visualize the plotted data
